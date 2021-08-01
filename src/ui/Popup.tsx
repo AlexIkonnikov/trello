144ddcbd98004b15673c55042b35146182e47878
Alex <sashaikonnikov55@gmail.com>
2021-08-01 23:00:57 +0600

    refactor: Moved Background and Overlay elements into the Popup element

--- src/ui/Popup.tsx
+++ src/ui/Popup.tsx
@@ -1,6 +1,5 @@
 import React, {FC} from 'react';
-import { Overlay } from './Overlay';
-import { Background } from './Background';
+import styled from 'styled-components';
 import { Button } from './Button';
 
 export const Popup: FC<PopupProps> = ({children, onClose}) => {
@@ -17,3 +16,24 @@ export const Popup: FC<PopupProps> = ({children, onClose}) => {
 interface PopupProps {
   onClose?: () => void
 }
+
+const Overlay = styled.div`
+    background: rgba(1, 1, 1, 0.6);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    min-height: 100vh;
+    width: 100%;
+    position: absolute;
+    z-index: 10;
+    top: 0;
+    left: 0;
+`;
+
+const Background = styled.div`
+    background: rgb(235, 236, 240);
+    width: auto;
+    border-radius: 5px;
+    padding: 10px;
+`;
+
